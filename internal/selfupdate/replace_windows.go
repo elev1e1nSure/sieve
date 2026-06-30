91c9e91a3586d67abc926f017f26aea9ec999dcf
elev1e1n <elev1e1n@gmail.com>
2026-06-30 20:01:46 +0300

    fix(selfupdate): avoid cmd quoting crash

--- internal/selfupdate/replace_windows.go
+++ internal/selfupdate/replace_windows.go
@@ -3,21 +3,38 @@
 package selfupdate
 
 import (
+	"fmt"
 	"os"
 	"os/exec"
+	"path/filepath"
 	"strings"
 )
 
 func replaceCurrentExecutable(exe, replacement string, restart bool) error {
-	args := []string{
-		"/C",
-		"ping 127.0.0.1 -n 2 > nul && move /Y " + quote(replacement) + " " + quote(exe) + " > nul",
+	script, err := os.CreateTemp("", "sieve-update-*.cmd")
+	if err != nil {
+		return err
 	}
+	scriptPath := script.Name()
+
+	content := "@echo off\r\n" +
+		"ping 127.0.0.1 -n 2 > nul\r\n" +
+		fmt.Sprintf("move /Y %s %s > nul\r\n", quote(replacement), quote(exe))
 	if restart {
-		args[1] += " && " + quote(exe)
+		content += fmt.Sprintf("start \"\" %s\r\n", quote(exe))
 	}
 
-	cmd := exec.Command("cmd", args...)
+	if _, err := script.WriteString(content); err != nil {
+		script.Close()
+		os.Remove(scriptPath)
+		return err
+	}
+	if err := script.Close(); err != nil {
+		os.Remove(scriptPath)
+		return err
+	}
+
+	cmd := exec.Command("cmd", "/C", filepath.Clean(scriptPath))
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
